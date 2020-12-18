30e29a0427b51785107b14b9f46afe5e3795bd4e
Darryl Wright <dwright04@qub.ac.uk>
2020-12-17 21:53:25 -0600

    Add comments to run.py

--- run.py
+++ run.py
@@ -8,26 +8,62 @@ from lasair_zooniverse import lasair_object
 import time
 import numpy as np
 ################################   
-             
-#base_dir = os.getenv('LASAIR_CONFIG_PATH')
-base_dir = '/Users/dwright/dev/Zooniverse_SLSN'
-config = ConfigParser()    
-config.read('%s/config.ini' % (base_dir))
-log = logging.getLogger("main-logger")
 
-try:
-    lasair_zoo = lasair_zooniverse_class(config.get('APP', 'KAFKA_SERVER'), config.get('APP', 'ENDPOINT'))
-    objectIds = lasair_zoo.query_lasair_topic(config.get('APP','GROUP_ID'), config.get('APP','TOPIC'))
-    if (config.get('APP','RECORDS_LIMIT')) != 'None':
-        max_limit = config.getint('APP','RECORDS_LIMIT')
-        objectIds = objectIds[:max_limit]
-    proto_subjects = []
-    for object_id in objectIds:
-        lasair_zoo.wget_objectdata(object_id, config.get('APP','URL'), config.get('APP','DATA_DIR'))
-        proto_subject = lasair_zoo.produce_proto_subject(object_id, config.get('APP','DATA_DIR'))
-        if (proto_subject != None):
-            proto_subjects.append(proto_subject)
-    lasair_zoo.create_subjects_and_link_to_project(proto_subjects,config.get('APP','PROJECT_ID'), config.get('APP','WORKFLOW_ID'), None)
-except Exception:
-    log.exception("Error in main block")
+def main():
+  # Read config file
+  config = ConfigParser()
+  config.read('./config.ini')
+  log = logging.getLogger("main-logger")
 
+  # Instantiate lasair-Zooniverse interface class
+  lasair_zoo = lasair_zooniverse_class(config.get('APP', 'KAFKA_SERVER'),
+                                       config.get('APP', 'ENDPOINT'))
+
+  # Query lasair kafka stream for objects according to group id and the
+  # topic (aka. Stream name at https://lasair.roe.ac.uk/streams/)
+  objectIds = lasair_zoo.query_lasair_topic(config.get('APP','GROUP_ID'),
+                                            config.get('APP','TOPIC'))
+
+  # If a limit on the number of objects to process is set, truncate the object
+  # list accordingly
+  if (config.get('APP','RECORDS_LIMIT')) != 'None':
+    max_limit = config.getint('APP','RECORDS_LIMIT')
+    objectIds = objectIds[:max_limit]
+
+  # Create an empty list to track the proto-subjects.
+  proto_subjects = []
+
+  # Iterate through the objects queried from lasair
+  for object_id in objectIds:
+    # Grab the lightcurve data for each subject from lasair
+    lasair_zoo.wget_objectdata(object_id,
+                               config.get('APP','URL'),
+                               config.get('APP','DATA_DIR'))
+  
+    # Create a proto-subject for this objects.  A proto-subject gathers the
+    # information required to construct a subject for the Zooniverse.  In this
+    # case the lasair_zoo.produce_proto_subject produces a lightcurve plot,
+    # from the data downloaded above from lasair.  This method also gets a
+    # PanSTARRS-1 image at the transient location with panstamps
+    # (https://github.com/thespacedoctor/panstamps).
+    proto_subject = \
+      lasair_zoo.produce_proto_subject(object_id, config.get('APP','DATA_DIR'))
+  
+    # If everything went well in the previous step add this to our list of
+    # proto-subjects
+    if (proto_subject != None):
+      proto_subjects.append(proto_subject)
+
+  # The lasair_zoo.create_subjects_and_link_to_project take the list of
+  # proto-subjects and produce subjects for the Zooniverse project.  The method
+  # also uploads the subjects to Zooniverse and links the to the project and
+  # workflow corresponding to the project and workflow ids provided in the
+  # config. In this example the subjects are linked to the Superluminous \
+  # Supernova project.
+  lasair_zoo.create_subjects_and_link_to_project(proto_subjects,
+                                                 config.get('APP','PROJECT_ID'),
+                                                 config.get('APP','WORKFLOW_ID'),
+                                                 None)
+
+if __name__ == '__main__':
+  main()
